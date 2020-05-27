7aa7a6ef334371813399945295cc269a1a61e6be
HiZweli <zwelihle@rapidtrade.biz>
2020-05-27 16:29:59 +0200

    Learning

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+// var firstName = 'Zweli';
+// console.log(firstName);
+
+const lastName = 'Rt';
+const age = 10;
+
+const fullAge = true;
+console.log(fullAge);
+
+
+const firstName = 'John';
+const ageTwo = 28;
+
+// Type coercion
+console.log(firstName + ' ' + ageTwo);
